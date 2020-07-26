12bf1f86cc9209e736d905b2ba660b4a2a603441
olga600312 <olga600312@gmail.com>
2020-07-26 10:17:36 +0300

    Commit test

--- src/main/java/global/App.java
+++ src/main/java/global/App.java
@@ -15,6 +15,7 @@ public class App {
     public static void main(String[] args) {
        Color c1=new Color(0xd7dfe1, true);
        Color c2=Utilities.color(c1,50,0.4f);
+
       System.err.println("Color c1:"+c1.getRGB()+" Color c2:"+c2.getRGB());
     }
 }
